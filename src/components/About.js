9b291f3c6267acf27e8bc352372333ddff43dcd6
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-06-02 12:42:21 -0700

    commit tree 4c953e55492cf82f8157e097c531bb3f9f8bf7a4

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,32 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  function isBio() {
+    if (props.bio === undefined || props.bio === '') {
+      return null
+    } 
+    return <p>{props.bio}</p>;
+  }
+  console.log(props.bio);
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {isBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
+
     </div>
+  );
+}
+
+function Links(props) {
+  return (
+    <>
+      <h3>Links</h3>
+      <a href="props.github">{props.github}</a>
+      <a href="props.linkedin">{props.linkedin}</a>
+    </>
   );
 }
 
